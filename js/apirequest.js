9c0d902a7a1263abe827247892fbc52eba790795
Jesy <jncepero@gmail.com>
2021-04-29 13:44:42 +0200

    Correcciones

--- js/apirequest.js
+++ js/apirequest.js
@@ -1,25 +1,15 @@
-
-
-//PRIMERO DEBO VER LOS DATOS
-//pedir los datos a la api
-
-
-//////ESTO ES PARA PINTAR EL LISTADO DE NOMBRES DE LA IZQUIERDA
-async function getAllPeople(pNumPag) {//esta funcion va a consultar la api, hayq ue hacer la peticion, que va a applicar un fetch
+async function getAllPeople(pNumPag) {
     const url = "https://swapi.dev/api/people/?page=" + pNumPag;
-    let request = await fetch(url, { method: 'GET' }) //ESTO ES UNA PROMESA. 
+    let request = await fetch(url, { method: 'GET' })
     let data = await request.json();
-    printAllPeople(data); //HAY QUE PINTAR LAS FUNCIONES EN EVENTOS, recordar que este script, esta en el head.
-    //el data devuelve todo el objeto
+    printAllPeople(data);
 }
 getAllPeople(1)
 
-
-///////ESTO ES PARA PINTRA LA INFORMACION DE UNA SOLA PEROSNA EN LA VENTANA DE LA DERECHA.
 async function getOnePerson(pUrl) {
     let request = await fetch(pUrl, { method: 'GET' });
     let data = await request.json();
-    printInfoPerson(data);//esto devuelve en la consola informacion del evento que pincho
+    printInfoPerson(data);
 }
 
 async function getFilm(pUrl) {
